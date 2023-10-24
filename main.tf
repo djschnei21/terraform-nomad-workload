154f96c11d10a10a11c3876e58df26729af28239
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-10-24 15:41:34 -0400

    don't need AWS

--- main.tf
+++ main.tf
@@ -1,15 +1,5 @@
 terraform {
   required_providers {
-    doormat = {
-      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
-      version = "~> 0.0.6"
-    }
-
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.8.0"
-    }
-
     vault = {
       source = "hashicorp/vault"
       version = "~> 3.18.0"
@@ -27,24 +17,10 @@ terraform {
   }
 }
 
-provider "doormat" {}
-
 provider "consul" {
   address = "${data.terraform_remote_state.hcp_clusters.outputs.consul_public_endpoint}:443"
   token = data.terraform_remote_state.hcp_clusters.outputs.consul_root_token
   scheme  = "https" 
-}
- 
-data "doormat_aws_credentials" "creds" {
-  provider = doormat
-  role_arn = "arn:aws:iam::365006510262:role/tfc-doormat-role_7_workload"
-}
-
-provider "aws" {
-  region     = var.region
-  access_key = data.doormat_aws_credentials.creds.access_key
-  secret_key = data.doormat_aws_credentials.creds.secret_key
-  token      = data.doormat_aws_credentials.creds.token
 }
 
 data "terraform_remote_state" "networking" {
@@ -226,21 +202,3 @@ EOH
 } 
 EOT
 }
-
-# resource "aws_lb_target_group" "frontend_tg" {
-#   name     = "frontend-tg"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = data.terraform_remote_state.networking.outputs.vpc_id
-# }
-
-# resource "aws_lb_target_group_attachment" "frontend_tg" {
-#   target_group_arn = aws_lb_target_group.frontend_tg.arn
-#   port             = 80
-#   target_id        = data.terraform_remote_state.nomad_nodes.outputs.nomad_client_x86_asg
-# }
-
-# // Optional Full KVM based VM Example
-# resource "nomad_job" "fullvm" {
-#  jobspec = file("${path.module}/nomad-jobs/mongodb-vmdk.hcl")
-# }
