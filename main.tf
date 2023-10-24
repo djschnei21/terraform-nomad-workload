c87332de21b7df842566159f53b67920a9d59454
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2023-10-24 11:26:27 -0400

    app_image from var

--- main.tf
+++ main.tf
@@ -165,7 +165,66 @@ resource "nomad_job" "frontend" {
   depends_on = [
     vault_database_secret_backend_role.mongodb
   ]
-  jobspec = file("${path.module}/nomad-jobs/frontend.hcl")
+  hcl2 {
+    vars = {
+      app_image = var.app_image
+    }
+  }
+  jobspec = <<EOT
+variable "app_image" {
+  type = string
+}
+job "demo-frontend" {
+    datacenters = ["dc1"]
+    node_pool = "x86"
+    type = "service"
+    
+    group "frontend" {
+        network {
+            mode = "bridge"
+
+            port "http" {
+                static = 3100
+                to     = 3100
+            }
+        }
+        service {
+            name = "demo-frontend"
+            port = "http"
+            address = "${attr.unique.platform.aws.public-ipv4}"
+
+            connect {
+                sidecar_service {
+                    proxy {
+                        upstreams {
+                            destination_name = "demo-mongodb"
+                            local_bind_port  = 27017
+                        }
+                    }
+                }
+            }
+        }
+        task "frontend" {
+            driver = "docker"
+            vault {
+                policies = ["nomad"]
+                change_mode   = "restart"
+            }
+            template {
+                data = <<EOH
+MONGOKU_DEFAULT_HOST={{ with secret "mongodb/creds/demo" }}{{ .Data.username }}:{{ .Data.password }}{{ end }}@127.0.0.1:27017
+EOH
+                destination = "secrets/mongoku.env"
+                env         = true
+            }
+
+            config {
+                image = var.app_image
+            }
+        }
+    }
+} 
+EOT
 }
 
 # resource "aws_lb_target_group" "frontend_tg" {
--- variables.tf
+++ variables.tf
@@ -1,13 +1,22 @@
 variable "stack_id" {
   type        = string
+  default = "hashistack"
   description = "The name of your stack"
 }
 
 variable "tfc_organization" {
   type    = string
+  default = "djs-tfcb"
+  description = "The TFCB organization to use"
 }
 
 variable "region" {
   type        = string
+  default = "us-east-2"
   description = "The AWS and HCP region to create resources in"
+}
+
+variable "app_image" {
+  type = string
+  description = "for demo: huggingface/mongoku:latest"
 }
